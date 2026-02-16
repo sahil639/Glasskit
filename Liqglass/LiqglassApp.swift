17a677190d212807cb9ff055a745e21679db4ee6
sahil <sahilpednekar332000@gmail.com>
2026-02-16 20:56:57 +0800

    Initial Commit

---
+++ Liqglass/LiqglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiqglassApp.swift
+//  Liqglass
+//
+//  Created by quminsoda on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LiqglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiqglassTests/LiqglassTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiqglassTests.swift
+//  LiqglassTests
+//
+//  Created by quminsoda on 16/02/26.
+//
+
+import Testing
+
+struct LiqglassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
